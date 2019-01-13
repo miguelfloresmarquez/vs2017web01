9c26256a4b03d13da0be1be37caf3ad3ade9e44a
miguelfloresmarquez <mfloresmarquez@outlook.com>
2019-01-13 13:17:37 -0500

    Mas

---
+++ Cibertec.UI.MVC/Cibertec.UI.MVC/Controllers/RegistroController.cs
@@ -0,0 +1,20 @@
+﻿using Cibertec.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cibertec.UI.MVC.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        [HttpPost]
+        public JsonResult Registro(PersonaViewModel persona)
+        {
+            persona.FullName = persona.Nombre + " " + persona.Apellidos;
+            return Json(persona);
+        }
+    }
+}
