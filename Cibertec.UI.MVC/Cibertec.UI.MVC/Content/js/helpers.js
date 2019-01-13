92ac0855ca1f09c5b7c250a2f1aa877483d195f3
miguelfloresmarquez <mfloresmarquez@outlook.com>
2019-01-13 12:20:28 -0500

    semana 4

---
+++ Cibertec.UI.MVC/Cibertec.UI.MVC/Content/js/helpers.js
@@ -0,0 +1,34 @@
+﻿(function (cibertec) {
+    cibertec.helpers = {
+        replaceAll: function (string, find, replace) {
+            var result = string.replace(
+                new RegExp(find, 'g'), replace
+            );
+            return result;
+        },
+        getAniosArray: function (anioInicio) {
+            var hoy = new Date();
+            var anios = [];
+            for (i = anioInicio; i <= hoy.getFullYear(); i++) {
+                anios.push(i);
+            }
+            return anios;
+        },
+        BloquearControls: function () {
+            $("input, select, textarea").attr("disabled", "disabled");
+            $("#idEnviar").attr("disabled", "disabled");
+        },
+        DesbloquearControls: function () {
+            $("input, select, button, textarea").removeAttr("disabled");
+        },
+        SetObjetLocalStorage: function (jsObject, keyStorage) {
+            var json = JSON.stringify(jsObject);
+            localStorage.setItem(keyStorage, json);
+        },
+        GetObjetLocalStorage: function (keyStorage) {
+            var json = localStorage.getItem(keyStorage);
+            return JSON.parse(json);
+        }
+    }
+}
+)(window.cibertec = window.cibertec || {});
